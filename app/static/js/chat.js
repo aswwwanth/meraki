d15e6a01bd5c43401b89116ed78f7f59a1e222a2
aswwwanth <aswanth@am.students.amrita.edu>
2020-05-11 19:49:35 +0530

    Chat UI improvement

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,14 +1,21 @@
 function scrollToBottom(){
     $(".chat-body").scrollTop($('.chat-body')[0].scrollHeight);
 }
-
+var lastMessenger = null;
 function addMessage(data, username){
     var message_processed;
-    if(data.username == username)
+    if(data.username == lastMessenger){
+        if(data.username == username)
+            message_processed = `<div class="message right-message" style="margin-top: -5px"><div class="message-container"><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
+        else
+            message_processed = `<div class="message left-message"><div class="message-container" style="margin-left: 55px; margin-top: -15px"><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
+    }
+    else if(data.username == username)
         message_processed = `<div class="message right-message"><div class="message-container"><div class="message-user">${data.username}</div><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
     else
         message_processed = `<div class="message left-message"><div class="message-profile"><img src="/static/images/profile/${data['username'].substr(0, 2).toUpperCase()}.png"></div><div class="message-container"><div class="message-user">${data.username}</div><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
     $('.chat-messages').append(message_processed);
+    lastMessenger = data.username;
     scrollToBottom();
 }
 
