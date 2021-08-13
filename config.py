8cbadec9eddef176d8ee7c16d4ab76340aba0cc8
Aswanth <aswanth366@gmail.com>
2021-08-13 20:01:01 +0530

    Dockerize application

--- config.py
+++ config.py
@@ -5,17 +5,10 @@ class Config(object):
         'user': 'meraki',
         'pw': 'meraki2020*',
         'db': 'meraki',
-        'host': 'localhost',
+        'host': 'db',
         'port': '5432',
     }
     
     SQLALCHEMY_DATABASE_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'meraki2020*'
-
-    # MAIL_SERVER = 'smtp.office365.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = 'aswin@am.students.amrita.edu'
-    # MAIL_PASSWORD = 'Hangover123'
-    # MAIL_DEFAULT_SENDER = 'aswin@am.students.amrita.edu'
