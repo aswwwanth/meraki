012c139fd9c93bcc278cb6dfe1717d9913f77c85
Aswanth <aswanth366@gmail.com>
2022-09-20 23:37:25 +0530

    comment console logs + prettier

--- app/static/js/chat.js
+++ app/static/js/chat.js
@@ -1,30 +1,43 @@
-function scrollToBottom(){
-    $(".chat-body").scrollTop($('.chat-body')[0].scrollHeight);
+function scrollToBottom() {
+  $(".chat-body").scrollTop($(".chat-body")[0].scrollHeight);
 }
 var lastMessenger = null;
-function addMessage(data, username){
-    var message_processed;
-    console.log(lastMessenger + " == " + data.username);
-    if(data.username == lastMessenger){
-        if(data.username == username)
-            message_processed = `<div class="message right-message" style="margin-top: -5px"><div class="message-container"><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
-        else
-            message_processed = `<div class="message left-message"><div class="message-container" style="margin-left: 55px; margin-top: -15px"><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
-    }
-    else if(data.username == username)
-        message_processed = `<div class="message right-message"><div class="message-container"><div class="message-user">${data.username}</div><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
+function addMessage(data, username) {
+  var message_processed;
+  // console.log(lastMessenger + " == " + data.username);
+  if (data.username == lastMessenger) {
+    if (data.username == username)
+      message_processed = `<div class="message right-message" style="margin-top: -5px"><div class="message-container"><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
     else
-        message_processed = `<div class="message left-message"><div class="message-profile"><img src="/static/images/profile/${data['username'].substr(0, 2).toUpperCase()}.png"></div><div class="message-container"><div class="message-user">${data.username}</div><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
-    $('.chat-messages').append(message_processed);
-    lastMessenger = data.username;
-    scrollToBottom();
+      message_processed = `<div class="message left-message"><div class="message-container" style="margin-left: 55px; margin-top: -15px"><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
+  } else if (data.username == username)
+    message_processed = `<div class="message right-message"><div class="message-container"><div class="message-user">${data.username}</div><div class="message-content">${data.message}</div><div class="message-time">${data.time}</div></div></div>`;
+  else
+    message_processed = `<div class="message left-message"><div class="message-profile"><img src="/static/images/profile/${data[
+      "username"
+    ]
+      .substr(0, 2)
+      .toUpperCase()}.png"></div><div class="message-container"><div class="message-user">${
+      data.username
+    }</div><div class="message-content">${
+      data.message
+    }</div><div class="message-time">${data.time}</div></div></div>`;
+  $(".chat-messages").append(message_processed);
+  lastMessenger = data.username;
+  scrollToBottom();
 }
 
 // Check scroll
-$(".chat-body").scroll(function() {
-    if($(this).scrollTop() + $(this).innerHeight() + 5 < $(this)[0].scrollHeight) {
-        $('.chat-footer').css('box-shadow', '0px -14px 25px -30px rgba(46,46,46,0.87)');
-    } else {
-        $('.chat-footer').css('box-shadow', '0px 0px 0px 0px transparent');
-    }
+$(".chat-body").scroll(function () {
+  if (
+    $(this).scrollTop() + $(this).innerHeight() + 5 <
+    $(this)[0].scrollHeight
+  ) {
+    $(".chat-footer").css(
+      "box-shadow",
+      "0px -14px 25px -30px rgba(46,46,46,0.87)"
+    );
+  } else {
+    $(".chat-footer").css("box-shadow", "0px 0px 0px 0px transparent");
+  }
 });
