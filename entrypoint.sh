9c5dfdcb97606d53783378f9c5173066a6d8ea5e
Aswanth <aswanth366@gmail.com>
2023-04-19 22:59:28 +0530

    change migration directory in prod

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
-# database migrations
-flask db init
-flask db migrate
-flask db upgrade
-
 # running the server
 if [ "$ENV" = "prod" ]; then
+    
+    # database migrations
+    flask db init --directory /migrations
+    flask db migrate --directory /migrations
+    flask db upgrade --directory /migrations
+
     service nginx start
     gunicorn --worker-class eventlet -w 1 --bind unix:meraki.sock -u www-data -m 007 wsgi:app
-else 
+else
+    # database migrations
+    flask db init
+    flask db migrate
+    flask db upgrade
+
     python3 meraki.py
 fi
